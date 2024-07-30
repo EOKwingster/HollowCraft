e49f57112d5fe1bb89b45acaec3cb087561c15aa
wingster <y1709175257@outlook.com>
2024-07-30 15:01:11 +0800

    Init mod

---
+++ src/main/java/com/eokwingster/eldenskills/EldenSkills.java
@@ -0,0 +1,17 @@
+package com.eokwingster.eldenskills;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(EldenSkills.MODID)
+public class EldenSkills
+{
+    public static final String MODID = "eldenskills";
+
+    public EldenSkills(IEventBus bus, ModContainer modContainer) {
+
+
+
+    }
+}
